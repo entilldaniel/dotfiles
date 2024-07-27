4bc0295d1b61803833a6845ee2462f4003b4480a
Daniel Figueroa <daniel.whalenut@gmail.com>
2024-07-27 20:34:49 +0200

    Minor corfu config.

--- shared/.config/emacs/init.el
+++ shared/.config/emacs/init.el
@@ -356,21 +356,17 @@
   (marginalia-mode))
 
 (use-package corfu
-  ;; Optional customizations
   :custom
   (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
   (corfu-auto t)                 ;; Enable auto completion
   (corfu-separator ?\s)          ;; Orderless field separator
   (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
   (corfu-quit-no-match t)        ;; Never quit, even if there is no match
-  (corfu-preview-current t)      ;; Disable current candidate preview
+  (corfu-preview-current t)      ;; Enable current candidate preview
   (corfu-preselect 'prompt)      ;; Preselect the prompt
   (corfu-on-exact-match nil)     ;; Configure handling of exact matches
   (corfu-scroll-margin 5)        ;; Use scroll margin
-  ;; Enable Corfu only for certain modes.
-  :hook ((prog-mode . corfu-mode)
-         (shell-mode . corfu-mode)
-         (eshell-mode . corfu-mode))
+  :hook ((prog-mode . corfu-mode))
 
   ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
   ;; be used globally (M-/).  See also the customization variable
