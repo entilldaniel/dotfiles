65dc7380074614857e370c86bbdf33132b4fd23d
Daniel Figueroa <daniel.figueroa@accounting.pe>
2025-03-10 22:45:37 +0100

    Changed default font preset.

--- shared/.config/emacs/init.el
+++ shared/.config/emacs/init.el
@@ -227,7 +227,7 @@
 
 (cond ((equal (system-name) "endive") (fontaine-set-preset 'large))
       ((equal (system-name) "archie") (fontaine-set-preset 'regular))
-      ((equal (system-name) "slartibartfast") (fontaine-set-preset 'huge))
+      ((equal (system-name) "slartibartfast") (fontaine-set-preset 'tight))
       ((equal "" "") (fontaine-set-preset 'regular)))
 
 (use-package ligature
